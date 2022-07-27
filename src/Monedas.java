0524df7a8dd4b3132e49aa773720520e550709a2
KevinxDx3 <85362446+KevinxDx3@users.noreply.github.com>
2022-07-27 12:32:13 -0500

    cambio

--- src/Monedas.java
+++ src/Monedas.java
@@ -6,7 +6,9 @@ public class Monedas {
     private JComboBox MONEDA1;
     private JComboBox MONEDA2;
     private JTextField textField1;
-    private JLabel Resultado;
+    private JList list1;
+    private JPanel mainPanel;
+    private JTextField resultadoTxt;
 
     public Monedas() {
 
@@ -15,12 +17,52 @@ public class Monedas {
         MONEDA1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Integer numero1;
+                Integer resultado;
+                double constante;
+
                 String moneda1= MONEDA1.getSelectedItem().toString();
-                numero1 = Integer.parseInt(textField1.getText());
+                String moneda2= MONEDA2.getSelectedItem().toString();
+                double numero1=Double.parseDouble(textField1.getText());
+
+
+                //USD (DOLAR AMERICANO)
+                //COP (PESO COLOMBIANO)
+                //EUR (EUROS)
+                //JPY (YENES JAP)
+                //VEF (BOLIVAR)
+
+                if (moneda1 == "USD" && moneda2 == "USD" ){
+
+                    constante=1;
+
+                } else if (moneda1 == "USD" && moneda2=="COP" ) {
+
+                    constante=4425.74;
+                } else if (moneda1 == "USD" && moneda2=="EUR") {
+
+                    constante=0.99;
+
+                } else if (moneda1 == "USD" && moneda2=="JPY") {
+
+                    constante= 137.27;
+
+                } else if (moneda1 == "USD" && moneda2 == "VEF") {
+
+                    constante=5.39;
+
+                }
 
             }
         });
 
+
+    }
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("Monedas");
+        frame.setContentPane(new Monedas().mainPanel);
+        frame.setTitle("Welcome");
+        frame.setSize(500,300);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
